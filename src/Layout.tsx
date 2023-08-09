65bbba2b3f007c455135211d20594e1fa30e4161
danuta8 <118287357+danuta8@users.noreply.github.com>
2023-08-09 14:33:58 -0400

    fixed prettier error

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -30,12 +30,12 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
       <Head>
         <title>{fields.pageTitle.value.toString() || 'Page'}</title>
         <link rel="icon" href={`${publicUrl}/favicon.ico`} />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" 
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
           rel="stylesheet"
           />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" 
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap"
           rel="stylesheet"
           />
         {headLinks.map((headLink) => (
